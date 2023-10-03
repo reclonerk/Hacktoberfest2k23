657bd502ab48cf69d038be2aed2e3abaa38e6fe0
Deepak Mina <95670537+Deepak5910@users.noreply.github.com>
2023-10-04 00:04:04 +0530

    Update color.py
    changed color name

--- color.py
+++ color.py
@@ -1,6 +1,6 @@
 from colorama import Fore, Back, Style
 
-print(Fore.RED + 'some red text')
+print(Fore.YELLOW + 'some red text')
 print(Back.GREEN + 'and with a green background')
 print(Style.DIM + 'and in dim text')
 print(Style.RESET_ALL)
